a60ea2bcee2e5078c72edf70eedd3301f9f6a956
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-16 19:07:16 -0600

    Creating express server and port to use

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 // DEPENDENCIES
 // npm packages to install
 const express = require("express");
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
