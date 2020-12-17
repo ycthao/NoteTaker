38f01361e7c03207aa82dd2be1ab7f1d957d900c
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-16 19:09:32 -0600

    Add listener to server and print message when sucessfully ran

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,9 @@ const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
