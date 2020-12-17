5e0da27288d17a26029e51c444413491ed6f153e
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-16 19:43:42 -0600

    Trying to call index.html to open

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,10 +8,15 @@ const app = express();
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
 
+const index = "../index.html";
+console.log(index);
 
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, index));
+});
 
 
 // Start server and logging message on which port it is listening
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
-  });
+});
