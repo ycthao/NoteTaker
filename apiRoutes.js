ea9c0f09f9044eca1da564d2a028c89a3a4b1039
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-18 23:23:09 -0600

    Fix post and delete

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,34 +1,55 @@
 // LOAD DATA
 
 const fs = require("fs");
-let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 // ROUTING
+module.exports = function (app) {
+    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-module.exports = function(app) {
+    app.get("/api/notes", function (req, res) {
+        res.json(notes);
+    });
 
-  app.get("/api/notes", function(req, res) {
-    res.json(notes);
-  });
+    app.get("/api/notes/:id", function (req, res) {
+        res.json(notes[Number(req.params.id)]);
+    });
 
-  app.get("/api/notes/:id", function(req, res) {
-    res.json(data[Number(req.params.id)]);
-  });
+    // API POST Requests---------------------------------------------------------------------------
 
-  // API POST Requests---------------------------------------------------------------------------
+    app.post("/api/notes", function (req, res) {
 
-  app.post("/api/notes", function(req, res) {
- 
-    
-  });
+        // let lastId = 0;
+        // let id = lastId + 1;
+
+        // notes.push({ id, ...req.body });
+        // res.json(notes.slice(-1));
+
+        let newNote = req.body;
+        let uniqueId = (notes.length).toString();
+
+        newNote.id = uniqueId;
+        notes.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes), function(err) {
+            if (err) throw (err);
+        }); 
+
+        res.json(notes);  
+
+    });
 
 
 
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
+    app.delete("/api/notes/:id", function (req, res) {
+        let noteID = req.params.id;
 
-    res.json({ ok: true });
-  });
+        notes = notes.filter(currentNote => {
+            return currentNote.id != noteID;
+        });
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes), function(err) {
+            if (err) throw (err);
+        }); 
+        res.end();
+    });
 };
