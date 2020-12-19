d428a5d84b00d8720ef070471995df147c55cdd9
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-18 23:24:03 -0600

    fix/erase blank space

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 // DEPENDENCIES
-
 const express = require("express");
-
 
 // Tells node that we are creating an "express" server
 const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('./'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ROUTER
-
 require("./apiRoutes")(app);
 require("./htmlRoutes")(app);
 
 // LISTENER
-
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
