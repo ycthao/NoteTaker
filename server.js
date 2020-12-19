f6730866597aefc1c9c9208e285fe01108be7f56
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-18 18:53:29 -0600

    adding depenndencies

---
+++ server.js
@@ -0,0 +1,6 @@
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
